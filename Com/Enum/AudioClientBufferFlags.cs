638dd37ea04b06694c32b879d443fbed962651d3
rodgers.r <rodgers.r@belgiumcampus.ac.za>
2019-03-15 11:06:59 +0200

    Initialize.

---
+++ Com/Enum/AudioClientBufferFlags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WASAPI.NET.Com
+{
+   [Flags]
+   public enum AudioClientBufferFlags
+   {
+      None = 0x0,
+      DataDiscontinuity = 0x1,
+      Silent = 0x2,
+      TimestampError = 0x3
+   };
+}
